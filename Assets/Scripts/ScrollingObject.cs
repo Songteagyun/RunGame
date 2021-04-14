bb1ed705f88423224f3e30aa4e87715281a01581
Songteagyun <80800089+Songteagyun@users.noreply.github.com>
2021-04-14 10:50:00 +0900

    0414 송태균

--- Assets/Scripts/ScrollingObject.cs
+++ Assets/Scripts/ScrollingObject.cs
@@ -7,6 +7,7 @@ public class ScrollingObject : MonoBehaviour
     public float speed = 10f;
     private void Update()
     {
+        if(!GameManager.instance.isGameover)
         transform.Translate(Vector3.left * speed * Time.deltaTime);
     }
 }
